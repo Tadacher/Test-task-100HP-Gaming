a6e41024834de3e9980fc7eb94158caf1c0224d8
Daniil Levin <dan3000pro@mail.ru>
2023-11-16 17:12:03 +0300

    Missile launch fix

--- Assets/Resources/Codebase/Services/Factories/MissileFactory.cs
+++ Assets/Resources/Codebase/Services/Factories/MissileFactory.cs
@@ -12,6 +12,8 @@ public class MissileFactory : AbstractMonoBehaviuorFactory<MissileBehaviour>
  
     public MissileFactory(MissilefactorySettings missilefactorySettings, AudioService audioService, ParticleFactory particleFactory) : base()
     {
+        _audioService = audioService;
+        _particleFactory = particleFactory;
         _missilePrefab = missilefactorySettings.missilePrefab;
         _speed = missilefactorySettings.missileSpeed;
     }
@@ -23,6 +25,7 @@ public class MissileFactory : AbstractMonoBehaviuorFactory<MissileBehaviour>
     {
         MissileBehaviour missileBehaviour = GameObject.Instantiate(_missilePrefab, null);
         missileBehaviour.Initialize(_speed, damage, _audioService, _particleFactory);
+        _audioService.PlayMissileLaunchSound();
         return missileBehaviour;
     }
 
